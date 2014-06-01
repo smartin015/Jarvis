536009b0fa5b8a0693c479e55912abd68e6b41f7
Scott Martin <smartin015@gmail.com>
2014-06-01 14:00:25 -0400

    Fixed up kaicong audio gst file

--- Inputs/KaicongAudioGst.py
+++ Inputs/KaicongAudioGst.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # Origin: http://cgit.freedesktop.org/gstreamer/gst-python/tree/examples/filesrc.py
 
-import sys
-import gobject 
-gobject.threads_init()
-
 import pygst
 pygst.require('0.10')
 import gst
@@ -35,17 +31,14 @@ class KaicongAudioSource(gst.BaseSrc):
 
         self.caps = gst.caps_from_string('audio/x-raw-int, rate=7600, endianness=1234, channels=1, width=16, depth=16, signed=true')
 
-        # TODO: Set as property
-        self.audio = KaicongAudio("192.168.1.15")
-        self.audio.connect()
-        gst.info("Connected audio")
-
     def set_property(self, name, value):
         if name == 'ip':
             self.audio = KaicongAudio(value)
             self.audio.connect()
+            gst.info("Connected audio")
 
     def do_create(self, offset, size):
+        assert self.audio
         data = self.audio.read()
         buf = gst.Buffer(data)
         buf.set_caps(self.caps)
@@ -55,10 +48,31 @@ class KaicongAudioSource(gst.BaseSrc):
 gobject.type_register(KaicongAudioSource)
 gst.element_register(KaicongAudioSource, 'kaicongaudiosrc', gst.RANK_MARGINAL)
 
+def gen_kaicong_audio_src(ip):
+  src = gst.element_factory_make("kaicongaudiosrc", "audiosrc")
+  src.set_property("ip", sys.argv[1])
+  return src
+
 if __name__ == "__main__":
-  pipeline_str = 'kaicongaudiosrc ! audioconvert ! audioresample ! autoaudiosink'
-  pipeline = gst.parse_launch(pipeline_str)
+  import sys
+  import gobject 
+  gobject.threads_init()
+
+  if len(sys.argv) != 2:
+    print "Usage: %s <ip_address>" % sys.argv[0]
+    sys.exit(-1)
+
+  pipeline = gst.Pipeline("pipe")
+
+  src = gen_kaicong_audio_src(sys.argv[1])
+  conv = gst.element_factory_make("audioconvert", "audioconv")
+  res = gst.element_factory_make("audioresample", "audioresamp")
+  sink = gst.element_factory_make("autoaudiosink", "audiosink")
+  
+  pipeline.add(src, conv, res, sink)
+  gst.element_link_many(src, conv, res, sink)
   pipeline.set_state(gst.STATE_PLAYING)
 
   main_loop = gobject.MainLoop()
   main_loop.run()
+  
