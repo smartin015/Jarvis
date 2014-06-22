4885a20d9bcbe2de4b73630fbed32f4325477a91
Scott Martin <smartin015@gmail.com>
2014-06-22 16:40:07 -0400

    Moved images into right and front folders, automated locations a bit

--- Holodeck/Effects/Locations.py
+++ Holodeck/Effects/Locations.py
@@ -1,21 +1,42 @@
 from Holodeck.Settings import Pipe as P
 from Outputs.RGBMultiController import NTOWER, NRING
 from Outputs.ScreenController import ScreenController as scl
-from Holodeck.Engine import EffectTemplate
+from Holodeck.Engine import EffectTemplate, classname_to_id
 from random import randint
 import time
 import inspect
 import sys
+IMG_PATH = "Holodeck/Images/"
 
 SKY = [20, 100, 205]
 SAND = [180, 140, 100]
 GRASS = [50, 125, 0]
 
+def flicker(rgb, flicker = 3):
+  randomNum = randint(0,2)
+
+  if randomNum == 0:
+    if rgb[0] <= 255 - flicker:
+      rgb[0] = rgb[0] + flicker
+    if rgb[1] <= 255 - flicker:
+      rgb[1] = rgb[1] + flicker
+    if rgb[2] <= 255 - flicker:
+      rgb[2] = rgb[2] + flicker
+  elif randomNum == 1:
+    if rgb[0] > 0 + flicker:
+      rgb[0] = rgb[0] - flicker
+    if rgb[1] > 0 + flicker:
+      rgb[1] = rgb[1] - flicker
+    if rgb[2] > 0 + flicker:
+      rgb[2] = rgb[2] - flicker
+
+  return rgb
 
 class LocationTemplate(EffectTemplate):
   TRANSITION_TIME = 120
-
-  def setup(self):
+  
+  def __init__(self, pipes, active_effects, remove_cb):
+    EffectTemplate.__init__(self, pipes, active_effects, remove_cb)
     self.steady_mapping = dict(
       [(k,c[0]) for (k,c) in self.get_mapping().items()]
     )
@@ -25,7 +46,9 @@ class LocationTemplate(EffectTemplate):
     self.tfloor = 0
     self.transition = False
     self.prev_window_bot = None
-
+    self.img_front = scl.loadimg(IMG_PATH + "front/%s.jpg" % classname_to_id(self.__class__.__name__))
+    self.img_right = scl.loadimg(IMG_PATH + "right/%s.jpg" % classname_to_id(self.__class__.__name__))
+  
   def location_mapping(self):
     raise Exception("Unimplemented")
 
@@ -96,7 +119,12 @@ class LocationTemplate(EffectTemplate):
       self.screen_transition = scl.gen_zoom(screen, final, self.transition_screen)
     return self.handle_screen_transition(final)
 
+  def wall_img_default(self, prev):
+    return self.img_right
 
+  def window_img_default(self, prev):
+    return self.img_front
+    
   def _get_mapping(self): 
     if not self.transition:
       priorities = dict([(k,c[1]) for (k,c) in self.get_mapping().items()])
@@ -114,17 +142,13 @@ class LocationTemplate(EffectTemplate):
 
 class ForestEffect(LocationTemplate):
 
-  def setup(self):
-    LocationTemplate.setup(self)
-    self.forest_screen = scl.loadimg("Holodeck/Images/forest.jpg")
-
   def get_mapping(self):
     return {
       P.FLOOR: (self.floor, 1),
       P.WINDOWTOP: (self.window_top, 1),
       P.WINDOWBOT: (self.window_bot, 1),
-      P.WALLIMG: (self.wall_img, 1),
-      P.WINDOWIMG: (self.window_img, 1),
+      P.WALLIMG: (self.wall_img_default, 1),
+      P.WINDOWIMG: (self.window_img_default, 1),
     }
 
   def floor(self, prev):
@@ -136,17 +160,7 @@ class ForestEffect(LocationTemplate):
   def window_bot(self, prev):
     return [102, 155, 0]
 
-  def wall_img(self, prev):
-    return self.forest_screen
-
-  def window_img(self, prev):
-    return self.forest_screen
-
 class PlainsEffect(LocationTemplate):
-
-  def setup(self):
-    LocationTemplate.setup(self)
-    self.plains_screen = scl.loadimg("Holodeck/Images/grassland.jpg")
 
   def get_mapping(self):
     return {
@@ -154,8 +168,8 @@ class PlainsEffect(LocationTemplate):
       P.TOWER: (self.tower, 1),
       P.WINDOWTOP: (self.window_top, 1),
       P.WINDOWBOT: (self.window_bot, 1),
-      P.WALLIMG: (self.wall_img, 1),
-      P.WINDOWIMG: (self.window_img, 1),
+      P.WALLIMG: (self.wall_img_default, 1),
+      P.WINDOWIMG: (self.window_img_default, 1),
     }
 
   def floor(self, prev):
@@ -172,64 +186,7 @@ class PlainsEffect(LocationTemplate):
     return [0, 128, 0]
     #return [0, 255, 255]
 
-  def wall_img(self, prev):
-    return self.plains_screen
-
-  def window_img(self, prev):
-    return self.plains_screen
-
-
-def flicker(rgb, flicker = 3):
-  randomNum = randint(0,2)
-
-  if randomNum == 0:
-    if rgb[0] <= 255 - flicker:
-      rgb[0] = rgb[0] + flicker
-    if rgb[1] <= 255 - flicker:
-      rgb[1] = rgb[1] + flicker
-    if rgb[2] <= 255 - flicker:
-      rgb[2] = rgb[2] + flicker
-  elif randomNum == 1:
-    if rgb[0] > 0 + flicker:
-      rgb[0] = rgb[0] - flicker
-    if rgb[1] > 0 + flicker:
-      rgb[1] = rgb[1] - flicker
-    if rgb[2] > 0 + flicker:
-      rgb[2] = rgb[2] - flicker
-
-  return rgb
-    
-
-
-class DesertEffect(EffectTemplate):
-
-  def tower(self, prev):
-    return [flicker(list(SKY)) for i in xrange(NTOWER)]
-
-
-  def get_mapping(self):
-    return {
-      P.FLOOR: (self.floor, 1),
-      P.TOWER: (self.tower, 1),
-      P.WINDOWTOP: (self.window_top, 1),
-      P.WINDOWBOT: (self.window_bot, 1),
-    }
-
-  def floor(self, prev):
-    return SAND
-
-  def window_top(self, prev):
-    return flicker(list(SKY))
-
-  def window_bot(self, prev):
-    return SAND
-
 class TundraEffect(LocationTemplate):
-
-  def setup(self):
-    LocationTemplate.setup(self)
-    self.img_front = scl.loadimg("Holodeck/Images/tundra.jpg")
-    self.img_right = scl.loadimg("Holodeck/Images/tundra_90.jpg")
 
   def tower(self, prev):
     return [(list(SKY)) for i in xrange(NTOWER)]
@@ -240,8 +197,8 @@ class TundraEffect(LocationTemplate):
       P.TOWER: (self.tower, 1),
       P.WINDOWTOP: (self.window_top, 1),
       P.WINDOWBOT: (self.window_bot, 1),
-      P.WALLIMG: (self.wall_img, 1),
-      P.WINDOWIMG: (self.window_img, 1),
+      P.WALLIMG: (self.wall_img_default, 1),
+      P.WINDOWIMG: (self.window_img_default, 1),
     }
 
   def floor(self, prev):
@@ -253,12 +210,59 @@ class TundraEffect(LocationTemplate):
   def window_bot(self, prev):
     return [255,255,255]
     
-  def wall_img(self, prev):
-    return self.img_right
+class RiverEffect(LocationTemplate):
 
-  def window_img(self, prev):
-    return self.img_front
+  def setup(self):
+    self.img_front = scl.loadimg(IMG_PATH + "river.jpg")
+    self.img_right = scl.loadimg(IMG_PATH + "tundra_90.jpg")
 
+  def tower(self, prev):
+    return [(list(SKY)) for i in xrange(NTOWER)]
+
+  def get_mapping(self):
+    return {
+      P.FLOOR: (self.floor, 1),
+      P.TOWER: (self.tower, 1),
+      P.WINDOWTOP: (self.window_top, 1),
+      P.WINDOWBOT: (self.window_bot, 1),
+    }
+
+  def floor(self, prev):
+    return [0,0,255]
+
+  def window_top(self, prev):
+    return SKY
+
+  def window_bot(self, prev):
+    return [0,0,255]
+    
+    
+class DesertEffect(EffectTemplate):
+
+  def get_mapping(self):
+    return {
+      P.FLOOR: (self.floor, 1),
+      P.TOWER: (self.tower, 1),
+      P.WINDOWTOP: (self.window_top, 1),
+      P.WINDOWBOT: (self.window_bot, 1),
+    }
+
+  def floor(self, prev):
+    return SAND
+    
+  def tower(self, prev):
+    return [flicker(list(SKY)) for i in xrange(NTOWER)]
+
+  def window_top(self, prev):
+    return flicker(list(SKY))
+
+  def window_bot(self, prev):
+    return SAND
+
+
+
+
+    
 class WaterEffect(EffectTemplate):
 
   def setup(self):
