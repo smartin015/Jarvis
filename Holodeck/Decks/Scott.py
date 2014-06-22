31f3901175227524148cb618b2019ff8035e0462
Scott Martin <smartin015@gmail.com>
2014-06-22 13:16:22 -0400

    Did the right thing

---
+++ Holodeck/Decks/Scott.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import logging
+import threading
+logging.basicConfig()
+logging.addLevelName( logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName( logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+
+from Holodeck.Settings import Pipe as P
+from Outputs.ScreenController import ScreenController
+from Outputs.AudioController import AudioController
+from Holodeck.Server import HolodeckServer
+import time
+
+class Holodeck(HolodeckServer):
+  def __init__(self):
+    self.devices = {
+      "proj": ScreenController(),
+      "audio": AudioController(),
+    }
+    self.last_sounds = []
+    self.last_img = None
+    self.img_path = "Assets/Images/"
+
+    HolodeckServer.__init__(self)
+
+  def get_pipeline_handlers(self):
+    return [
+      ([P.WINDOWIMG], self.window_scrn),
+      ([P.SOUND], self.sound),
+    ]
+
+  def get_pipeline_defaults(self):
+    return {
+      P.WINDOWIMG:  ScreenController.get_black_image(),
+      P.SOUND:      [], 
+    }
+
+  def window_scrn(self, scrn):
+    self.devices['proj'].set_scrn(scrn)
+     
+  def sound(self, sounds=[]):
+    for s in sounds:
+      if s not in self.last_sounds:
+        self.devices['audio'].play(s)
+    for s in self.last_sounds:
+      if s not in sounds:
+        self.devices['audio'].fadeout(s)
+    self.last_sounds = sounds
+
+if __name__ == "__main__":
+  h = Holodeck() 
+  h.serve_forever()
+
