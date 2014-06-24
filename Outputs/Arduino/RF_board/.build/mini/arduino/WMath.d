9f53479d8223d99f30a9483c14daa4ec42d646fa
Jarvis PC <toddmedema@gmail.com>
2014-06-24 14:28:57 -0400

    Backup commit

--- Outputs/Arduino/RF_board/.build/mini/arduino/WMath.d
+++
@@ -1,1 +0,0 @@
-.build/mini/arduino/WMath.d .build/mini/arduino/WMath.o: /usr/share/arduino/hardware/arduino/cores/arduino/WMath.cpp
--- Outputs/Arduino/RF_board/.build/mini/arduino/WString.d
+++
@@ -1,2 +0,0 @@
-.build/mini/arduino/WString.d .build/mini/arduino/WString.o: /usr/share/arduino/hardware/arduino/cores/arduino/WString.cpp \
- /usr/share/arduino/hardware/arduino/cores/arduino/WString.h
