5050ce7bdd1dd67224c102e21797d71750184445
Jarvis PC <toddmedema@gmail.com>
2014-07-09 10:26:14 -0400

    Removed tracked arduino build folders

--- Outputs/Arduino/ACController/.build/nano328/arduino/WMath.d
+++
@@ -1,1 +0,0 @@
-.build/nano328/arduino/WMath.d .build/nano328/arduino/WMath.o: /usr/share/arduino/hardware/arduino/cores/arduino/WMath.cpp
--- Outputs/Arduino/ACController/.build/nano328/arduino/WString.d
+++
@@ -1,2 +0,0 @@
-.build/nano328/arduino/WString.d .build/nano328/arduino/WString.o: /usr/share/arduino/hardware/arduino/cores/arduino/WString.cpp \
- /usr/share/arduino/hardware/arduino/cores/arduino/WString.h
